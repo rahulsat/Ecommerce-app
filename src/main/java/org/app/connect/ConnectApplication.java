f93f6a9d2a014a7c6bbdee57c81efba1886892a8
rahulsat <rahulsatla5656@gmail.com>
2026-05-29 19:47:54 +0530

    Initial commit

--- src/main/java/org/app/connect/ConnectApplication.java
+++ src/main/java/org/app/connect/ConnectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConnectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ConnectApplication.class, args);
+        System.out.println("Application is running");
     }
 
 }
